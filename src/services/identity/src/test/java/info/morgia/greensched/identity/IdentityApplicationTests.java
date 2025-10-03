fca4a840a29f57f6fccc113f2f77167e432799be
Emidio Morgia <emidio@morgia.info>
2025-10-03 09:21:48 +0200

    feat: initialize Spring Boot application with Maven wrapper and basic configuration

---
+++ src/services/identity/src/test/java/info/morgia/greensched/identity/IdentityApplicationTests.java
@@ -0,0 +1,13 @@
+package info.morgia.greensched.identity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
